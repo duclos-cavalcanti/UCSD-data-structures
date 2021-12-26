d4abd6db06418b9533757479c80fa04fd5383a11
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2021-12-26 06:04:11 +0100

    Added tests and test script :)

---
+++ basics/ex_3/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for f in $(ls tests); do
+  if [[ "${#f}" -eq 2 ]]; then
+    if [ -e bin/bin ]; then
+      cat tests/${f} | ./bin/bin > result.txt
+      result="$(diff result.txt tests/${f}.a --color=always)"
+      if [ -z "$result" ]; then
+        echo "Test: ${f}: SUCCESS"
+        echo "-------------------------"
+      else
+        echo "Test: ${f}: FAILED"
+        echo "YOURS"
+        cat result.txt
+        
+        echo ""
+
+        echo "CORRECT"
+        cat tests/${f}.a
+      fi
+    fi
+  fi
+done
+
+[ -f result.txt ] && rm result.txt
